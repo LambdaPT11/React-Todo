01a1eac8daccc469d5040939e67b2fa249952928
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-01-22 10:40:52 -0500

    form layed out

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+    return(
+        <div>
+            <input
+            placeholder='Todo'
+            ></input>
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default Form;
